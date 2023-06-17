b13f03888f4bf359a9af09ef0ca5836127425d59
JPEdgar <Jon.P.Edgar@gmail.com>
2023-06-17 14:01:56 -0700

    added a disable for the submit button

--- src/components/elements/SubmitButton.jsx
+++ src/components/elements/SubmitButton.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import { Button } from "react-bootstrap";
 
-import { useGuessData } from "../../hooks";
+import { useGuessData, useLoadingFlag } from "../../hooks";
 
 const SubmitButton = ({ handleSubmit }) => {
     const { guessData } = useGuessData();
+    const {isLoadingFlag} = useLoadingFlag()
     return (
         <div className="w-100 ms-4">
-            <Button onClick={handleSubmit} className="w-100 " disabled={guessData.editFlag}>
+            <Button onClick={handleSubmit} className="w-100 " disabled={guessData.editFlag || isLoadingFlag}>
                 Submit
             </Button>
         </div>
