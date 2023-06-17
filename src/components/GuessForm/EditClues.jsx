cc1b90021fb833dfd7e0e1d87ac83f342a6dec2b
JPEdgar <Jon.P.Edgar@gmail.com>
2023-06-16 18:11:53 -0700

    Getting the edit row set up

--- src/components/GuessForm/EditClues.jsx
+++ src/components/GuessForm/EditClues.jsx
@@ -13,28 +13,58 @@ const EditClues = ({ data, index }) => {
     console.log(data);
 
     const handleClick = (attemptIndex, letterIndex) => {
-        let tempAttemptList = [...data.attemptList];
-        const tempLetterObj = tempAttemptList[attemptIndex][letterIndex];
+        // let tempAttemptList = [...data.attemptList];
+        // const tempLetterObj = tempAttemptList[attemptIndex][letterIndex];
 
-        if (tempLetterObj.code === "x") tempLetterObj.code = "y";
-        else if (tempLetterObj.code === "y") tempLetterObj.code = "g";
-        else tempLetterObj.code = "x";
+        // if (tempLetterObj.code === "x") tempLetterObj.code = "y";
+        // else if (tempLetterObj.code === "y") tempLetterObj.code = "g";
+        // else tempLetterObj.code = "x";
 
-        tempAttemptList[attemptIndex][letterIndex] = tempLetterObj;
+        // tempAttemptList[attemptIndex][letterIndex] = tempLetterObj;
 
         // setData((curr) => ({ ...curr, attemptList: tempAttemptList }));
     };
 
     const handleToggle = (attemptListIndex, letterObjectIndex) => {
-        const revisedAttemptList = [...data.attemptList];
-        revisedAttemptList[attemptListIndex][letterObjectIndex].edit = true;
+        // const revisedAttemptList = [...data.attemptList];
+        // revisedAttemptList[attemptListIndex][letterObjectIndex].edit = true;
         // setData((curr) => ({ ...curr, attemptList: revisedAttemptList, }));
+    };
+
+    const handleBack = () => {
+        // const revisedAttemptList = [...data.attemptList];
+        // revisedAttemptList.pop();
+        // setData((curr) => ({ ...curr, editFlag: true, attemptList: revisedAttemptList, }));
+    };
+
+    
+    const handleSubmit = (e) => {
+        // e.preventDefault();
+        // let letterArray = [];
+        // let codeArray = [];
+        // const returnArray = [];
+        // const { attemptList } = data;
+        
+        // for (let i = 0; i < attemptList.length; i++) {
+        //     for (let j = 0; j < attemptList[i].length; j++) {
+        //         console.log(attemptList[i][j]);
+        //         letterArray.push(attemptList[i][j].letter);
+        //         codeArray.push(attemptList[i][j].code);
+        //     }            
+            
+        //     returnArray.push({ word, clue });
+        //     letterArray = [];
+        //     codeArray = [];
+        // }
+        
+        // sendWord(returnArray);
+        // setData((curr) => ({ ...curr, editFlag: true, }));
     };
 
     return (
         <>
             <Stack direction="horizontal" gap={2}>
-                <BackButton handleBack={null} />
+                <BackButton handleBack={handleBack} />
                 {data.map((letterData, letterIndex) =>
                     letterData.edit ? (
                         <></>
@@ -49,7 +79,7 @@ const EditClues = ({ data, index }) => {
                         />
                     )
                 )}
-                <SubmitButton handleSubmit={null} />
+                <SubmitButton handleSubmit={handleSubmit} />
             </Stack>
         </>
     );
