87e9f8495c198ccaac19b540a41c37dac64dc238
JPEdgar <Jon.P.Edgar@gmail.com>
2023-06-16 17:02:23 -0700

    Added lodash to project and created cloneDeep utility

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import createAttemptObject from "./createAttemptObject";
 import convertArrayToString from "./convertArrayToString";
 import checkIfString from "./checkIfString";
+import cloneDeep from "./cloneDeep";
 
-export { createAttemptObject, convertArrayToString, checkIfString };
+export { createAttemptObject, convertArrayToString, checkIfString, cloneDeep };
