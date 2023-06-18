6c6fbcc2e0d672401cac8ae3cb29acf04374f36a
JPEdgar <Jon.P.Edgar@gmail.com>
2023-06-17 18:10:37 -0700

    Removed background color with incorrect color name -- need to remove at some point

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="text-center w-100" style={{ backgroundColor: "pink" }}>
+        <div className="text-center w-100" style={{ backgroundColor: "pink-" }}>
             Wordle Bot
         </div>
     );
