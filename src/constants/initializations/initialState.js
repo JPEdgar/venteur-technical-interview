a4bb08fffb78f80ae7ca3834363860570f698b40
JPEdgar <Jon.P.Edgar@gmail.com>
2023-06-17 18:10:17 -0700

    Removed background color with incorrect color name -- need to remove at some point

--- src/constants/initializations/initialState.js
+++ src/constants/initializations/initialState.js
@@ -1,9 +1,16 @@
 const initialState = {
-    guessList: [], // [{word: string, clue: string}]
+    guessList: [], // an array specifically formatted for the api: [{word: string, clue: string}]
     isLoadingFlag: false,
     error: "",
     botSuggestion: "",
-    guessData: { editFlag: true, input: "", attemptList: [] },
+    guessData: {
+        editFlag: true,
+        input: "",
+        attemptList: [], // an array of objects:  [{letter: string, code: string, edit: bool}]
+        greenList: [], // an array of objects with the "g" code:  [{index: int, letter: string}]
+        yellowList: [], // an array of objects with the "y" code:  [{index: int, letter: string}]
+        blackList: "",  // a string of letters with the "x" code
+    },
 };
 
 export default initialState;
